1a2d49fcbeb44c86d1591c35045de4124d186499
DOTG29 <dgoris@edenschool.fr>
2023-03-31 16:01:22 +0200

    fini dark mode

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 let dark = document.getElementById("dark");
 
+// dark.addEventListener('click', () => {
+//     document.body.innerHTML = "<h1>ha ha c'est une blague</h1>"
+//     document.body.style.color = '#fff';
+//     document.body.style.backgroundColor = 'black';
+// })
 
 dark.addEventListener('click', () => {
-
+    document.body.style.color = '#fff';
+    document.body.style.backgroundColor = '';
 })
